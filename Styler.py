63573b5521dc6065457a16de1063952f31405c5e
Skrimpton <64572787+Skrimpton@users.noreply.github.com>
2024-02-22 19:00:28 +0100

    Update Styler.py

--- Styler.py
+++ Styler.py
@@ -25,27 +25,9 @@ C002                    = "#002839"
 C013                    = "#01392d"
 C3f4                    = "#3f452a" # GREENISH BROWNISH
 
-BUTTON_DISABLED_FG      = C55f
-SIDEBAR_COLOR           = C101 # DARK GREY
-MAINFRAME_COLOR         = C000 # BLACK
 WINDOW_COLOR            = C000 # BLACK
-TEXT_COLOR              = Cba8 # YELLOWISH ORANGE
-ENTRY_HIGH_BG           = C222 # DARK GREY
-ENTRY_HIGH_COLOR        = C444 # LIGHTER DARK GREY
-# ENTRY_HIGH_BG           = "#ffaa00"
-# FOREGROUND_ENTRY        = "#ffaa00"
-ENTRY_FOCUSED           = C55f
+ENTRY_FOCUSED           = Cffa
 ENTRY_UNFOCUSED         = Cfff
-
-
-SCROLLBAR_THROUGHCOLOR  = C000
-
-
-ODD_COLOR               = C181
-EVEN_COLOR              = C101
-ITEM_SEL_FOCUS          = C013
-ITEM_SEL_NOTFOCUS       = C002
-ITEM_SEL_FOCUS_TXT      = Cfff
 
 class Styler:
 
@@ -58,8 +40,7 @@ class Styler:
         # self.style.theme_use          ("clam")                            # use "clam"-theme
         # self.style.theme_use          ("classic")                         # use "classic"-theme
         # self.style.theme_use          ("vista")                           # use "vista"-theme (not available on linux)
-        # self.style.element_create     ("Plain.Field", "from", "clam")     # create element
-        self._style_entry                ()                                  #
+        self._style_entry               ()                                  
 
     def _style_entry(self):
 
@@ -80,29 +61,28 @@ class Styler:
         self.style.map(         'TEntry',
 
             foreground          = [ ('!focus',ENTRY_UNFOCUSED), ('focus',ENTRY_FOCUSED)    ],
-            background          = [ ('disabled',C333),('focus',C3f4), ('!focus',C000)    ],
-            bordercolor         = [ ('disabled',C444),('focus',C444), ('!focus',C000)    ],
-            insertcolor         = [ ('focus','#ffffff'),('!focus','#ffff1c')    ],
-            fieldbackground     = [ ('disabled',C333),('focus',C000),('!focus',C000)    ],
+            background          = [ ('disabled',C333),('focus',C3f4), ('!focus',WINDOW_COLOR)    ],
+            bordercolor         = [ ('disabled',C444),('focus',C444), ('!focus',WINDOW_COLOR)    ],
+            insertcolor         = [ ('focus',Cfff),('!focus','#ffff1c')    ],
+            fieldbackground     = [ ('disabled',C333),('focus',WINDOW_COLOR),('!focus',WINDOW_COLOR)    ],
         );
 
         self.style.map(         'TLabel',
-            background          = [ ('disabled',C000),('active',"#101010"), ('!active',"#000000") ],
+            background          = [ ('disabled',WINDOW_COLOR),('active',C101), ('!active',WINDOW_COLOR) ],
             foreground          = [ ('disabled',C101),('!active',Cfff),('active',Cffa)    ],
         );
         self.style.map(         'TButton',
             foreground          = [ ('disabled',C101),('!active',Cfff),('active',Cffa)    ],
-            background          = [ ('disabled',C333),('active',"#101010"), ('!active',"#000000") ],
-            relif               = [('disabled','sunken'),('!disabled','flat')],
+            background          = [ ('disabled',C333),('active',C101), ('!active',WINDOW_COLOR) ],
         );
 
         self.style.configure(   'TLabel',
-            background=C000,
+            background=WINDOW_COLOR,
             foreground=Cfff,
 
         );
         self.style.configure(   'TFrame',
-            background="#000000",
+            background=WINDOW_COLOR,
         );
 
 
