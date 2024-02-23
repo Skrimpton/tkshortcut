455075256d720156f3147d6ba0aae2269e1dbbda
Skrimpton <64572787+Skrimpton@users.noreply.github.com>
2024-02-23 13:52:47 +0100

    Update main.py
    Changes in importlib.util revealed problems with the original
    Simplified it to try except else

--- main.py
+++ main.py
@@ -1,49 +1,10 @@
-#!/bin/env python3
-
 import sys
-
-def error_exit(module=""):
-    if module == "":
-        module = "A"
-
-    print(f":: IMPORT ERROR :: in {__file__}\n{module} is not installed")
-    sys.exit(1)
-
-try:
-    import importlib
-except:
-    error_exit("'importlib'")
-
-# Check rquirements
-c_tk        = importlib.util.find_spec('tkinter')
-c_signal    = importlib.util.find_spec('signal')
-
-if c_tk == None:
-    error_exit("'tkinter'")
-elif c_signal == None:
-    error_exit("'signal'")
-else:
-    # import signal
-    from MainWindow import MainWindow
-
-
-# --------------------------------------------------------------------------
-#                                                          HANDLE <CTRL + C>
-# def keyboard_interrupt_handler(sig, frame):
-#     print(':: MAIN :: You pressed Ctrl+C!',"Exiting...")
-#     sys.exit(0)
-
+try:    import tkinter as tk
+except: print("no tkinter :("),sys.exit(1)
+else:   from MainWindow import MainWindow
 # --------------------------------------------------------------------------
 #                                                  MAKE AND SPAWN NEW WINDOW
-
-
 if __name__ == "__main__":
 
-    # signal.signal(
-    #     signal.SIGINT,
-    #     keyboard_interrupt_handler
-    # );
-
     app             = MainWindow()
-    # app.self_ref    = app
     app.show()
