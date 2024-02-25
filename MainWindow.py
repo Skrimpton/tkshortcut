c77526397b408212e6af339d45b83d69ca49d519
Skrimpton <64572787+Skrimpton@users.noreply.github.com>
2024-02-25 22:37:24 +0100

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -23,14 +23,14 @@ class MainWindow:
         keyboard_interrupt_handler
     );
     def __init__(self): #                   ### BEGIN ###
-
+        self.passed_file                    = None
         self.ctrl_c_timer                   = None
         self.style_toggled                  = False
         # --- Set root
         self.root                           = tk.Tk()
         self.styler                         = Styler(self.root) # comment out this to remove styling
         self.root.title                     ("Create Desktop Shortcut")
-        self.root.minsize                   (280,160)
+        self.root.minsize                   (300,180)
         # --- Hook up x-button to quit-function. Not needed, but maybe a nifty snippet.
         self.root.protocol                  ( "WM_DELETE_WINDOW", self.quit );
         # ---
@@ -40,143 +40,186 @@ class MainWindow:
     # ------------------------------------------------------------------------------
     #                                                         Construction functions
     def declareUi(self):
-        self.main_frame                     = ttk.Frame     (   self.root)
-        self.titleframe                     = ttk.Frame     (   self.main_frame)
-
-        self.buttonframe                    = ttk.Frame     (   self.main_frame)
-        self.frame                          = ttk.Frame     (   self.main_frame)
-
-        self.namelabel                      = ttk.Label     (   self.titleframe, text = "Title:",justify='left')
-        self.nametext                       = TweakedEntry  (   self.titleframe,font=('Arial',10))
+        if self.passed_file == None:
+            self.main_frame                     = ttk.Frame     (   self.root)
+            self.filenameframe                  = ttk.Frame     (   self.main_frame)
+            self.buttonframe                    = ttk.Frame     (   self.main_frame)
+            self.frame                          = ttk.Frame     (   self.main_frame)
 
 
-        self.filenamelabel                  = ttk.Label     (   self.frame, text = "Filename *:",justify='left',state='disabled')
-        self.filenametext                   = TweakedEntry  (   self.frame,font=('Arial',10),enabled=False)
-        self.filenametext.bind              ('<<FieldChanged>>',self.on_changes_name)
+            self.filenamelabel                  = ttk.Label     (   self.filenameframe, text = "Filename *:",justify='left',state='disabled')
+            self.filenametext                   = TweakedEntry  (   self.filenameframe,font=('Arial',10),enabled=False)
+            self.filenametext.bind              ('<<FieldChanged>>',self.on_changes_name)
 
-        self.choose_filename_button         = ttk.Button    (   self.frame, text = "Choose",
-                                                                command    = self.choose_filename
-                                            );
+            self.choose_filename_button         = ttk.Button    (   self.filenameframe, text = "Choose",
+                                                                    command    = self.choose_filename
+                                                );
 
-        self.execlabel                      = ttk.Label     (   self.frame, text = "Path to Executable *:",justify='left')
-        self.exectext                       = TweakedEntry  (   self.frame,font=('Arial',10))
-        self.exectext.bind                  ('<<FieldChanged>>',self.on_changes_exec)
-
-
-        self.iconlabel                      = ttk.Label     (   self.frame, text = "Icon:")
-        self.icontext                       = TweakedEntry  (   self.frame,font=('Arial',10))
-        self.choose_icon_button             = ttk.Button    (   self.frame, text="Choose",
-                                                                command = self.choose_icon
-                                            );
-
-        self.choosebutton                   = ttk.Button    (   self.frame, text="Choose",
-                                                                command = self.choose_file
-                                            );
+            self.namelabel                      = ttk.Label     (   self.frame, text = "Title:",justify='left')
+            self.nametext                       = TweakedEntry  (   self.frame,font=('Arial',10))
 
 
-        self.okbutton                       = ttk.Button    (   self.buttonframe, text="Ok",state='disabled',
+            self.outlabel                      = ttk.Label     (   self.frame, text = "Output dir:",justify='left')
+            self.outtext                       = TweakedEntry  (   self.frame,font=('Arial',10))
+            self.outtext.bind                  ('<<FieldChanged>>',self.on_changes_exec)
+            self.btn_sel_out                   = ttk.Button    (   self.frame, text="Choose",
+                                                                    command = self.choose_savepath
+                                                );
+
+            self.execlabel                      = ttk.Label     (   self.frame, text = "Path to Executable *:",justify='left')
+            self.exectext                       = TweakedEntry  (   self.frame,font=('Arial',10))
+            self.exectext.bind                  ('<<FieldChanged>>',self.on_changes_exec)
+            self.btn_sel_exec                   = ttk.Button    (   self.frame, text="Choose",
+                                                                    command = self.choose_file
+                                                );
+
+            self.iconlabel                      = ttk.Label     (   self.frame, text = "Icon:")
+            self.icontext                       = TweakedEntry  (   self.frame,font=('Arial',10))
+            self.choose_icon_button             = ttk.Button    (   self.frame, text="Choose",
+                                                                    command = self.choose_icon
+                                                );
+
+
+        self.btn_ok                       = ttk.Button    (   self.buttonframe, text="Ok",state='disabled',
                                                                 command = self.click_ok
                                             );
         self.cancelbutton                   = ttk.Button    (   self.buttonframe, text="Cancel",
                                                                 command = self.quit
                                             );
+        self.buildStylegroups()
+    def buildStylegroups(self):
+        for f in (  self.main_frame,
+                    self.filenameframe,
+                    self.buttonframe,
+                    self.frame,
+        ):
+            self.styler.stylegroup_frames.append(f)
+
+        for b in (  self.choose_icon_button,
+                    self.btn_sel_out,
+                    self.btn_sel_exec,
+                    self.btn_ok,
+                    self.cancelbutton,
+                    self.choose_filename_button,
+        ):
+            self.styler.stylegroup_buttons.append(b)
+
+        for e in (  self.filenametext,
+                    self.nametext,
+                    self.outtext,
+                    self.exectext,
+                    self.icontext,
+        ):
+            self.styler.stylegroup_entries.append(e)
+
+        for l in (
+            self.namelabel,
+            self.outlabel,
+            self.filenamelabel,
+            self.execlabel,
+            self.iconlabel,
+        ):
+            self.styler.stylegroup_labels.append(l)
+
+
+
     def toggle_style(self,_):
         self.style_toggled = not self.style_toggled
 
         if self.style_toggled:
 
-            self.main_frame             .configure(style='C.TFrame')
-            self.titleframe             .configure(style='C.TFrame')
-            self.buttonframe            .configure(style='C.TFrame')
-            self.frame                  .configure(style='C.TFrame')
-            self.choose_icon_button     .configure(style='C.TButton')
-            self.choosebutton           .configure(style='C.TButton')
-            self.okbutton               .configure(style='C.TButton')
-            self.cancelbutton           .configure(style='C.TButton')
-            self.choose_filename_button .configure(style='C.TButton')
-            self.namelabel              .configure(style='C.TLabel')
-            self.filenamelabel          .configure(style='C.TLabel')
-            self.execlabel              .configure(style='C.TLabel')
-            self.iconlabel              .configure(style='C.TLabel')
-            self.filenametext           .configure(style='C.TEntry')
-            self.nametext               .configure(style='C.TEntry')
-            self.exectext               .configure(style='C.TEntry')
-            self.icontext               .configure(style='C.TEntry')
+
+            for f in self.styler.stylegroup_frames:
+                f.configure(style='C.TFrame')
+
+            for l in self.styler.stylegroup_labels:
+                l.configure(style='C.TLabel')
+
+            for e in self.styler.stylegroup_entries:
+                e.configure(style='C.TEntry')
+
+            for b in self.styler.stylegroup_buttons:
+                b.configure(style='C.TButton')
+
 
         else:
-            self.main_frame             .configure(style='TFrame')
-            self.titleframe             .configure(style='TFrame')
-            self.buttonframe            .configure(style='TFrame')
-            self.frame                  .configure(style='TFrame')
-            self.choose_icon_button     .configure(style='TButton')
-            self.choosebutton           .configure(style='TButton')
-            self.okbutton               .configure(style='TButton')
-            self.cancelbutton           .configure(style='TButton')
-            self.choose_filename_button .configure(style='TButton')
-            self.namelabel              .configure(style='TLabel')
-            self.filenamelabel          .configure(style='TLabel')
-            self.execlabel              .configure(style='TLabel')
-            self.iconlabel              .configure(style='TLabel')
-            self.filenametext           .configure(style='TEntry')
-            self.nametext               .configure(style='TEntry')
-            self.exectext               .configure(style='TEntry')
-            self.icontext               .configure(style='TEntry')
+
+            for f in self.styler.stylegroup_frames:
+                f.configure(style='TFrame')
+
+            for l in self.styler.stylegroup_labels:
+                l.configure(style='TLabel')
+
+            for e in self.styler.stylegroup_entries:
+                e.configure(style='TEntry')
+
+            for b in self.styler.stylegroup_buttons:
+                b.configure(style='TButton')
+
 
     def do_geometry_management(self):
 
         # --- GRID
-        self.titleframe.columnconfigure     (1, weight=1)
+        self.filenameframe.columnconfigure  (1, weight=1)
         self.buttonframe.columnconfigure    (0, weight=1)
         self.buttonframe.columnconfigure    (1, weight=1)
         self.frame.columnconfigure          (1, weight=1)
 
-        self.namelabel.grid                 (row=0, column=0,sticky="we",           pady=3)
-        self.nametext.grid                  (row=0, column=1,sticky="we",   padx=3, pady=3)
+        self.filenamelabel.grid             (row=0, column=0,sticky="we",           pady=3)
+        self.filenametext.grid              (row=0, column=1,sticky="we",   padx=3, pady=3)
 
-        self.filenamelabel.grid             (row=0, column=0,sticky="we")
-        self.filenametext.grid              (row=0, column=1,sticky="we",   padx=3,         columnspan=2,)
+        self.execlabel.grid                 (row=0, column=0,sticky="we")
+        self.exectext.grid                  (row=0, column=1,sticky="we",   padx=3)
+        self.btn_sel_exec.grid              (row=0, column=2,               padx=3)
+
+        self.outlabel.grid                  (row=1, column=0,sticky="we",           pady=1)
+        self.outtext.grid                   (row=1, column=1,sticky="we",   padx=3, pady=1)
+        self.btn_sel_out.grid               (row=1, column=2,               padx=3, pady=1)
+
+        self.namelabel.grid                 (row=2, column=0,sticky="we",           pady=3)
+        self.nametext.grid                  (row=2, column=1,sticky="we",   padx=3, pady=3, columnspan=2,)
 
 
-        self.execlabel.grid                 (row=1, column=0,sticky="we",           pady=1)
-        self.exectext.grid                  (row=1, column=1,sticky="we",   padx=3, pady=1)
-        self.choosebutton.grid              (row=1, column=2,               padx=3, pady=1)
+        self.iconlabel.grid                 (row=3, column=0,sticky="we")
+        self.icontext.grid                  (row=3, column=1,sticky="we",   padx=3)
+        self.choose_icon_button.grid        (row=3, column=2,               padx=3)
 
-        self.iconlabel.grid                 (row=2, column=0,sticky="we")
-        self.icontext.grid                  (row=2, column=1,sticky="we",   padx=3)
-        self.choose_icon_button.grid        (row=2, column=2,               padx=3)
-
-        self.okbutton.grid                  (row=0, column=0,sticky="we")
+        self.btn_ok.grid                    (row=0, column=0,sticky="we")
         self.cancelbutton.grid              (row=0, column=1,sticky="we")
 
         # --- PACK
         self.main_frame.pack                (fill='both',expand=1)
-        self.titleframe.pack                (fill="x")
+        self.filenameframe.pack             (fill="x")
         self.frame.pack                     (fill="x",pady=5)
         self.buttonframe.pack               (fill="x",side='bottom')
 
     # ------------------------------------------------------------------------------
     #                                                           Functional functions
 
-
     def on_changes_exec(self,event=None): # event info of no value
 
         if os.path.isfile(self.exectext.entry_text.get()):
+
             self.filenametext.configure     (state='enabled')
             self.filenamelabel.configure    (state='enabled')
-            content = os.path.basename      (self.exectext.entry_text.get())
-            filename_minus_extension = os.path.splitext(content)[0]
-            content = f"{filename_minus_extension}.desktop"
+
+            content                     = os.path.basename(self.exectext.entry_text.get())
+            filename_minus_extension    = os.path.splitext(content)[0]
+            content                     = f"{filename_minus_extension}.desktop"
+
             self.filenametext.delete        (0, tk.END)
             self.filenametext.insert        (0, content)
+
         else:
             self.filenametext.configure     (state='disabled')
             self.filenamelabel.configure    (state='disabled')
 
     def on_changes_name(self,event=None): # event info of no value
+
         if self.filenametext.entry_text.get() != "":
-            self.okbutton.configure         (state='enabled')
+            self.btn_ok.configure           (state='enabled')
         else:
-            self.okbutton.configure         (state='disabled')
+            self.btn_ok.configure           (state='disabled')
 
 
     def quit(self):
@@ -191,15 +234,19 @@ class MainWindow:
     def click_ok(self):
         filename                            = self.filenametext.get()
         if os.path.isfile(self.exectext.get()):
+            # if self.outtext.entry_text.get() != "":
             parent_dir_of_executable            = os.path.dirname(os.path.realpath(self.exectext.get()))
 
-            if not os.access(parent_dir_of_executable, os.W_OK):
-                self.root.after(10,lambda: messagebox.showerror(title="Desktop-file creator:",
-                                     message="Directory is not writable")
-                );
-                return
-
-            filename = os.path.join(parent_dir_of_executable,f"{filename}.desktop")
+            # if not os.access(parent_dir_of_executable, os.W_OK):
+            #      # https://docs.python.org/3/library/os.html#os.access
+            #     self.root.after(10,lambda: messagebox.showerror(title="Desktop-file creator:",
+            #                          message="Directory is not writable")
+            #     );
+            #     return
+            if filename.endswith(".desktop"):
+                filename = os.path.join(parent_dir_of_executable,filename)
+            else:
+                filename = os.path.join(parent_dir_of_executable,f"{filename}.desktop")
 
             try:
                 with open(filename, "w") as my_file:
@@ -214,6 +261,11 @@ class MainWindow:
                 print(msg)
                 self.root.destroy               ()
 
+            except PermissionError as e:
+                # https://docs.python.org/3/library/os.html#os.access
+                self.root.after(10,lambda: messagebox.showerror(title="Desktop-file creator:",
+                                     message=f"Permission Error! Can't write to:\n{parent_dir_of_executable}")
+                );
             except:
                 self.root.after(10,lambda: messagebox.showerror(title="Desktop-file creator:",
                                      message="SHORTCUT WAS NOT CREATED")
@@ -223,6 +275,11 @@ class MainWindow:
     def choose_file(self):
         self.exectext.delete                (0, tk.END)
         self.exectext.insert                (0, fd.askopenfilename())
+        # self.exectext.entry_text.set(fd.askopenfilename())
+
+    def choose_savepath(self):
+        self.outtext.delete                (0, tk.END)
+        self.outtext.insert                (0, fd.askdirectory())
         # self.exectext.entry_text.set(fd.askopenfilename())
 
     def choose_filename(self):
